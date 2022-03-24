222ced24a010d58f74ac7a80dd9ba8c625abb073
cristinaj1 <cristinacee305@gmail.com>
2022-03-24 20:35:52 +0100

    ejercicio comenzado

---
+++ src/main/java/hospital/Empleado.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital;
+
+/**
+ *
+ * @author Cris
+ */
+public abstract class Empleado extends Persona {
+
+    //Atributos
+    private String numeroSeguridadSocial;
+    private double salario;
+
+    //MÉTODOS
+    //Calcula el IRPF(abstracto)
+    abstract double calcularIRPF();
+
+    //Constructores
+    public Empleado() {
+    }
+
+    public Empleado(String numeroSeguridadSocial, double salario) {
+        this.numeroSeguridadSocial = numeroSeguridadSocial;
+        this.salario = salario;
+    }
+
+    public Empleado(String numeroSeguridadSocial, double salario, String nombre, String apellidos, NIF nif) {
+        super(nombre, apellidos, nif);
+        this.numeroSeguridadSocial = numeroSeguridadSocial;
+        this.salario = salario;
+    }
+
+    //Getter y setters
+    public String getNumeroSeguridadSocial() {
+        return numeroSeguridadSocial;
+    }
+
+    public void setNumeroSeguridadSocial(String numeroSeguridadSocial) {
+        this.numeroSeguridadSocial = numeroSeguridadSocial;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Empleado{" + "numeroSeguridadSocial=" + numeroSeguridadSocial + ", salario=" + salario + '}';
+    }
+
+}
---
+++ src/main/java/hospital/Persona.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Cris
+ */
+public abstract class Persona {
+
+    private String nombre;
+    private String apellidos;
+    private NIF nif;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellidos, NIF nif) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public NIF getNif() {
+        return nif;
+    }
+
+    public void setNif(NIF nif) {
+        this.nif = nif;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", nif=" + nif + '}';
+    }
+
+    public void reovarNif(LocalDate fechaSolicitud) {
+        getNif().renovar(fechaSolicitud);
+    }
+}
